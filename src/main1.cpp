4e70deae6a9bef45b26f709acef2fb3550e2af6e
DarkWayC0de <darkwayc0de@gmail.com>
2019-03-06 14:32:19 +0000

    Añade la nueva ordenacion de los archivos
    Se ordena correctamente los archivos se crea un nuevo main para
    implenetar la practica 2 y se mueven los TODO a su corespondiente
    practica, tambien se modifica el cmakelists para que quede correctamente
    funcionando con el main de la practica 2

--- src/main1.cpp
+++ src/main1.cpp
@@ -7,23 +7,9 @@
 #define BS 2
 #define BS1 16
 #define BS2 8
-//TODO: En lugar de utilizar un​ vector<T>, el atributo se declara como un puntero a ​T
-//TODO: Constructor reserva y destructor elimina memoria dinamica
-//TODO: Constructor de copia y sobrecarga de asignacion
 
-//TODO: Añadir atributo de signo
-//TODO: El constructor acepta positivos y negativo las operaciones aceptan negativos y positivos
-//TODO: En caso de binario se empleara el complemento a2 con el signo en el bit mas significativo COn especializacion
-//TODO: Implentacion de las operaciones para complemnto 2
-
-//TODO: Crear clase NumberException que deriva de exception de la lib stdar
-//TODO: Exepciones para cuando se intenta mostrar numero con base mañor de 16 y para cuando el numero se sale de rango
-
-//TODO: Main para comprobar que funciona todo sobretodo binario dec y hex
-//TODO: Se solicitaran numeros, realizara operaciones y mostrara por pantalla los resultados
-//TODO: En caso excepcion derivada de NumberException, se informa al usuario y se continua con la ejecucion
 int main() {
-try {/*
+try {/*  MAIN PR1
     std::cout<<"Introduce el numero a codificar con "<<NU<<" caracteres en la base "<<BS<<" :";
     int a;
     std::cin>>a;
@@ -149,6 +135,7 @@ try {/*
     }
 
 */
+//MAIN MODIF PR1
   Number<NU, BS> A(1);
   Number<NU1, BS1> B(2);
   Number<NU2, BS2> C(7);
---
+++ src/main2.cpp
@@ -0,0 +1,32 @@
+//
+// Created by darkwaycode on 4/03/19.
+// Main pr2
+//
+
+#include <iostream>
+#include "../include/Number.h"
+#define NU 12
+#define NU1 12
+#define NU2 12
+
+#define BS 2
+#define BS1 16
+#define BS2 8
+//TODO: En lugar de utilizar un​ vector<T>, el atributo se declara como un puntero a ​T
+//TODO: Constructor reserva y destructor elimina memoria dinamica
+//TODO: Constructor de copia y sobrecarga de asignacion
+
+//TODO: Añadir atributo de signo
+//TODO: El constructor acepta positivos y negativo las operaciones aceptan negativos y positivos
+//TODO: En caso de binario se empleara el complemento a2 con el signo en el bit mas significativo COn especializacion
+//TODO: Implentacion de las operaciones para complemnto 2
+
+//TODO: Crear clase NumberException que deriva de exception de la lib stdar
+//TODO: Exepciones para cuando se intenta mostrar numero con base mañor de 16 y para cuando el numero se sale de rango
+
+//TODO: Main para comprobar que funciona todo sobretodo binario dec y hex
+//TODO: Se solicitaran numeros, realizara operaciones y mostrara por pantalla los resultados
+//TODO: En caso excepcion derivada de NumberException, se informa al usuario y se continua con la ejecucion
+int main() {
+
+}
